ce58bcc7a537fb1148b26e05cc687e93e026efea
liurrong <ypwenxiang@outlook.com>
2020-12-31 11:28:47 +0800

    base

---
+++ src/page/CesiumDemo/CesiumDemo.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./CesiumDemo.css";
+
+function CesiumDemo() {
+    return (
+        <div>
+            hello CesiumDemo
+        </div>
+    )
+}
+
+export default CesiumDemo;
